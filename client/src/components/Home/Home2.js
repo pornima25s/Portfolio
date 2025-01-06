69427ddcaa32129bfebf0c9aaee642d27e853cf8
pornima25s <sadanshivsudhir794@gmail.com>
2025-01-07 00:28:30 +0530

    Update Home page

---
+++ client/src/components/Home/Home2.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+    AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+    return (
+        <Container fluid className="home-about-section" id="about">
+            <Container>
+                <Row>
+                    <Col md={8} className="home-about-description">
+                        <h1 style={{ fontSize: "2.6em" }}>
+                            LET ME <span className="purple"> INTRODUCE </span> MYSELF
+                        </h1>
+                        <p className="home-about-body">
+                            Enthusiastic Web Developer with a passion for creating
+                            <i>
+                                <b className="purple">user-friendly, visually appealing websites and applications. </b>
+                            </i>
+                            <br />
+                            <br />
+                            Proficient in <b className="purple">HTML, CSS, JavaScript, and UI design</b>,
+                            with backend skills in <i><b className="purple">Node.js</b> and <b className="purple">Express.js</b></i>.
+                            <br />
+                            <br />
+                            A team player who enjoys working on collaborative projects and is always eager to
+                            <i>
+                                <b className="purple"> learn, bring fresh ideas, and improve </b>
+                            </i>
+                            while staying current with
+                            <b className="purple"> new web technologies. </b>
+                        </p>
+
+                    </Col>
+                    <Col md={4} className="myAvtar">
+                        <Tilt>
+                            <img src={myImg} className="img-fluid" alt="avatar" />
+                        </Tilt>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12} className="home-about-social">
+                        <h1>FIND ME ON</h1>
+                        <p>
+                            Feel free to <span className="purple">connect </span>with me
+                        </p>
+                        <ul className="home-about-social-links">
+                            <li className="social-icons">
+                                <a
+                                    href="https://github.com/pornima25s"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="icon-colour  home-social-icons"
+                                >
+                                    <AiFillGithub />
+                                </a>
+                            </li>
+                            <li className="social-icons">
+                                <a
+                                    href=""
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="icon-colour  home-social-icons"
+                                >
+                                    <AiOutlineTwitter />
+                                </a>
+                            </li>
+                            <li className="social-icons">
+                                <a
+                                    href="https://www.linkedin.com/in/pornima-sadanshiv32b17b25b/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="icon-colour  home-social-icons"
+                                >
+                                    <FaLinkedinIn />
+                                </a>
+                            </li>
+                            <li className="social-icons">
+                                <a
+                                    href="https://www.instagram.com/po.rnima_s25"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="icon-colour home-social-icons"
+                                >
+                                    <AiFillInstagram />
+                                </a>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
+}
+export default Home2;
