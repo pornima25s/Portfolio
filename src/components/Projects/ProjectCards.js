eb423febf061a107b8f10e806dfb5413789950d3
pornima25s <sadanshivsudhir794@gmail.com>
2025-04-15 00:21:43 +0530

    added projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,40 +1,92 @@
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button";
+// import { CgWebsite } from "react-icons/cg";
+// import { BsGithub } from "react-icons/bs";
+
+// function ProjectCards(props) {
+//   return (
+//     <Card className="project-card-view">
+//       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+//       <Card.Body>
+//         <Card.Title>{props.title}</Card.Title>
+//         <Card.Text style={{ textAlign: "justify" }}>
+//           {props.description}
+//         </Card.Text>
+//         <Button variant="primary" href={props.ghLink} target="_blank">
+//           <BsGithub /> &nbsp;
+//           {props.isBlog ? "Blog" : "GitHub"}
+//         </Button>
+//         {"\n"}
+//         {"\n"}
+
+//         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+
+//         {!props.isBlog && props.demoLink && (
+//           <Button
+//             variant="primary"
+//             href={props.demoLink}
+//             target="_blank"
+//             style={{ marginLeft: "10px" }}
+//           >
+//             <CgWebsite /> &nbsp;
+//             {"Demo"}
+//           </Button>
+//         )}
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+// export default ProjectCards;
+
+
+
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import { motion } from "framer-motion";
 
-function ProjectCards(props) {
+// This assumes you're passing an array of projects as a prop named `projects`
+function ProjectCards({ projects }) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
-      </Card.Body>
-    </Card>
+    <div className="d-flex flex-wrap justify-content-center gap-4">
+      {projects.map((p, index) => (
+        <motion.div
+          key={index}
+          className="project-card-view"
+          whileHover={{ scale: 1.04, rotate: 2 }}
+          transition={{ duration: 0.15, ease: "easeInOut" }}
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+        >
+          <Card className="h-200 shadow-lg" style={{ width: "32rem", background: "#32303a", color: "white" }}>
+            <Card.Img variant="top" src={p.imgPath} alt="card-img" className="rounded-top" />
+            <Card.Body>
+              <Card.Title>{p.title}</Card.Title>
+              <Card.Text style={{ textAlign: "justify", color: "#afb0b6" }}>
+                {p.description}
+              </Card.Text>
+              <div className="d-flex flex-wrap align-items-center gap-2">
+                <Button variant="primary" href={p.ghLink} target="_blank">
+                  <BsGithub /> &nbsp;
+                  {p.isBlog ? "Blog" : "GitHub"}
+                </Button>
+                {!p.isBlog && p.demoLink && (
+                  <Button variant="primary" href={p.demoLink} target="_blank">
+                    <CgWebsite /> &nbsp;
+                    Demo
+                  </Button>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        </motion.div>
+      ))}
+    </div>
   );
 }
+
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,55 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import ProjectCard from "./ProjectCards";
+// import Particle from "../Particle";
+
+// function Projects() {
+//   return (
+//     <Container fluid className="project-section">
+//       <Particle />
+//       <Container>
+//         <h1 className="project-heading">
+//           My Recent <strong className="purple">Works </strong>
+//         </h1>
+//         <p style={{ color: "white" }}>
+//           Here are a few projects I've worked on recently.
+//         </p>
+//         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+//         </Row>
+//       </Container>
+//     </Container>
+//   );
+// }
+
+// export default Projects;
+
+
+
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+import { Container, Row} from "react-bootstrap";
+import ProjectCards from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
+// Example data for projects
+const projects = [
+  {
+    title: "Project 1",
+    description: "This is a description of Project 1.",
+    imgPath: "/path/to/image1.jpg",
+    ghLink: "https://github.com/project1",
+    demoLink: "https://project1demo.com",
+    isBlog: false,
+  },
+  {
+    title: "Project 2",
+    description: "This is a description of Project 2.",
+    imgPath: "/path/to/image2.jpg",
+    ghLink: "https://github.com/project2",
+    demoLink: "https://project2demo.com",
+    isBlog: false,
+  },
+  // Add more projects here
+];
 
 function Projects() {
   return (
@@ -21,6 +63,8 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          {/* Pass the projects array as a prop to ProjectCards */}
+          <ProjectCards projects={projects} />
         </Row>
       </Container>
     </Container>
